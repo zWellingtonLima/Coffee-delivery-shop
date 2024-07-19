37f38f0bb4331f863e088bfacc0147eea3f90b4b
zWellingtonLima <0wellingtonlima1@gmail.com>
2024-07-19 18:15:06 +0100

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import { GlobalStyle } from "./styles/global";
+
+function App() {
+  return (
+    <>
+    <GlobalStyle />
+      <main></main>
+    </>
+  );
+}
+
+export default App;
