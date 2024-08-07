70d2e4421774f591ac9b15e112d952b97d6d97c5
zWellingtonLima <0wellingtonlima1@gmail.com>
2024-08-07 10:29:56 +0100

    feat: done Success page

--- src/pages/Success/index.tsx
+++ src/pages/Success/index.tsx
@@ -1,4 +1,4 @@
-import { MapPin, Timer } from "phosphor-react";
+import { CurrencyDollar, MapPin, Timer } from "phosphor-react";
 
 import { Container, Heading, Info, InfoContent, Order } from "./styles";
 import { useCart } from "../../hooks/useCart";
@@ -8,6 +8,15 @@ export function Success() {
   const { orders } = useCart();
   const { orderId } = useParams();
   const orderInfo = orders.find((order) => order.id === Number(orderId));
+  const paymentMethod = {
+    credit: "Cartão de crédito",
+    debit: "Cartão de débito",
+    cash: "Dinheiro",
+  };
+
+  if (!orderInfo?.id) {
+    return null;
+  }
 
   return (
     <Container>
@@ -18,31 +27,48 @@ export function Success() {
         </Heading>
 
         <Info>
-          <div>
-            <InfoContent>
+          <InfoContent>
+            <div>
               <MapPin size={32} />
-            </InfoContent>
+
+              <div>
+                <span>
+                  Entrega em{" "}
+                  <strong>
+                    {orderInfo?.street}, {orderInfo?.number}
+                  </strong>
+                </span>
+
+                <span>
+                  {orderInfo?.neighborhood} - {orderInfo?.city},{" "}
+                  {orderInfo?.state}
+                </span>
+              </div>
+            </div>
 
             <div>
-              <span>
-                Entrega em{" "}
-                <strong>
-                  {orderInfo?.street}, {orderInfo?.number}
-                </strong>
-              </span>
+              <Timer size={32} />
 
-              <span>
-                {orderInfo?.neighborhood} - {orderInfo?.city},{" "}
-                {orderInfo?.state}
-              </span>
+              <div>
+                <span>Previsão de entrega</span>
+                <strong>20 min - 30 min</strong>
+              </div>
             </div>
-          </div>
 
-          <div>
-            <Timer size={32}/>
-          </div>
+            <div>
+              <CurrencyDollar size={32} />
+
+              <div>
+                <span>Pagamento na entrega</span>
+
+                <strong>{paymentMethod[orderInfo.paymentMethod]}</strong>
+              </div>
+            </div>
+          </InfoContent>
         </Info>
       </Order>
+
+      <img src="/delivery.svg" alt="Pedido concluído" />
     </Container>
   );
 }
